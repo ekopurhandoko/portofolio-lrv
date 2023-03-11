aacfc53b8d5416fe2f803ea48b0b3c970d802c57
ekopurhandoko <80388970+ekopurhandoko@users.noreply.github.com>
2023-03-11 23:23:26 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\authController;
+use Laravel\Socialite\Facades\Socialite;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//sudah login dan belum logout teteap berada di dashboard
+Route::redirect('home','dashboard');
+
+//kehalaman index dan cek sudah login atau belum
+Route::get('/auth',[authController::class,"index"])->name('login')->middleware('guest');
+
+//halaman autentikasi
+Route::get('/auth/redirect',[authController::class,"redirect"])->middleware('guest');
+
+//kehalaman callback
+Route::get('/auth/callback',[authController::class,"callback"])->middleware('guest');
+
+//kehalaman dasboard
+Route::get('/dashboard', function(){
+    return 'Selamat datang '. Auth::user()->name .' di halaman dashboard';
+})->middleware('auth');
+
+//logout
+Route::get('/auth/logout',[authController::class,"logout"]);
